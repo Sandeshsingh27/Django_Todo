365028a39adc7dbfc3190e7389df708f79ac56b2
Sandesh Singh <sandeshsingh295@gmail.com>
2023-04-28 00:20:14 +0530

    added success alert

--- home/views.py
+++ home/views.py
@@ -4,14 +4,16 @@ from home import models
 # Create your views here.
 
 def index(request):
+    context={'success':False}
     if request.method=='POST':
         title=request.POST['title']
         desc=request.POST['desc']
 
         data=models.Task(title=title, desc=desc)
         data.save()
+        context={'success':True}
         print('user saved')
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def tasks(request):
     return render(request, 'tasks.html')
