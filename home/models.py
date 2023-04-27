4ef66d776481e29580dfa25e2ccdf0719795e1f8
Sandesh Singh <sandeshsingh295@gmail.com>
2023-04-27 23:39:23 +0530

    created models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=50)
+    desc=models.TextField()
+
+# Below function is used to edit the object filed shown in the admin interface
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, HttpResponse
-# from .models import 
+from home import models
 
 # Create your views here.
 
 def index(request):
+    if request.method=='POST':
+        title=request.POST['title']
+        desc=request.POST['desc']
+
+        data=models.Task(title=title, desc=desc)
+        data.save()
+        print('user saved')
     return render(request, 'index.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
