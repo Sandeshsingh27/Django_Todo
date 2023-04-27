9e872d73fe76816011027565d0e157b251be1d04
Sandesh Singh <sandeshsingh295@gmail.com>
2023-04-28 00:09:37 +0530

    added time in model

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class Task(models.Model):
     title=models.CharField(max_length=50)
     desc=models.TextField()
+    time=models.DateTimeField(auto_now_add=True, null=True)
 
 # Below function is used to edit the object filed shown in the admin interface
     def __str__(self):
